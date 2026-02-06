3c709ede60968ad8d58cb86a62b3072525c3d650
salahram390-sudo <salahram390@gmail.com>
2026-02-06 22:52:30 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = "meshwarak-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/login.html",
+  "/rider.html",
+  "/driver.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
